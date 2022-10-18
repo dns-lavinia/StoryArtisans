a7c2e5c8ddaa70b735050328125785a5b05e834a
adelincarabas <adelinromana@gmail.com>
2022-10-18 11:13:33 +0300

    added 2 routes and wrote a message on default route

--- server.js
+++ server.js
@@ -3,6 +3,9 @@ const Role = db.role;
 
 const dbConfig = require("./app/dbconfig/db.config");
 
+const express = require("express");
+const app = express();
+
 db.mongoose
   .connect(
     `mongodb+srv://${dbConfig.HOST}:${dbConfig.PASSWORD}@${dbConfig.DB}.8ayyvw3.mongodb.net/?retryWrites=true&w=majority`,
@@ -18,3 +21,16 @@ db.mongoose
     console.error("Connection error", err);
     process.exit();
   });
+
+app.get("/", (req, res) => {
+  res.json({ message: "Story Artisans - Carabas Adelin & Dinis Lavinia" });
+});
+
+// routes
+require("./app/routes/login.routes")(app);
+require("./app/routes/user.routes")(app);
+
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}.`);
+});
