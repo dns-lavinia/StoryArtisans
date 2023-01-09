08773833f38d6fe89be35f45bc090b126486bea1
Andreea-Lavinia DINIS <dns.lavinia@gmail.com>
2023-01-09 17:42:51 +0200

    Worked on profile screen

--- src/screens/ProfileScreen.js
+++ src/screens/ProfileScreen.js
@@ -1,28 +1,62 @@
 import React, { useEffect, useState, useContext } from "react";
-import { View, StyleSheet, Text } from "react-native";
+import { View, StyleSheet, Text, TouchableOpacity, Image } from "react-native";
+import { ScrollView } from "react-native-gesture-handler";
 
 // Local components import
 import DarkBackgroundS from "../components/atoms/DarkBackgroundS";
 import { AuthContext } from "../context/auth";
+import { BookContext } from "../context/book";
 
 import { theme } from '../core/theme';
 
-export default function ProfileScreen () {
+export default function ProfileScreen ({ navigation }) {
     const [email, setEmail] = useState("");
     const [username, setUsername] = useState("");
     const [state, setState] = useContext(AuthContext);
+    const [readingList, setReadingList] = useState(0);
+    const [works, setWorks] = useState(0);
+    const [books, setBooks] = useContext(BookContext);
+
+    let fakebooks = ([  {title: "book1"}, 
+                        {title: "book2"}]);
 
     useEffect(() => {
         if (state) {
             if(state.user === null) {
                 return;
             }
-
+            
+            // Show the username and email of the logged user
             const {id, username, email} = state.user
             setUsername(username);
             setEmail(email);
+
+            // Show the reading list and number of works
+            var query = {id: id};
+            // const { booksInfo } = await axios.get("http://10.0.2.2:8080/api/profile-books", query);
+
+            setReadingList(2);
+            setWorks(1);
+
+            fetchBooks(id);
         }
     }, [state]);
+
+    const fetchBooks = async (id) => {
+        // to get the parameters passed in the SearchScreen -> route.params
+        var query = {id: id};
+        console.log(query);
+
+        // const { bookData } = await axios.get("http://10.0.2.2:8080/api/my-books", query);
+        // setBooks(bookData);
+    };
+
+    const viewBook = (item) => {
+        navigation.navigate("BookViewScreen", {
+            title: item.title,
+            author: item.username,
+        });
+    }
 
     return (
         <DarkBackgroundS>
@@ -42,19 +76,39 @@ export default function ProfileScreen () {
 
                 <View style={styles.statsContainer}>
                     <Text style={styles.stats}>
-                        <Text>2 {'\n'}</Text>
+                        <Text>{readingList} {'\n'}</Text>
                         <Text >READING LIST</Text>
                     </Text>
 
                     <Text style={styles.stats}>
-                        <Text>1 {'\n'}</Text>
+                        <Text>{works} {'\n'}</Text>
                         <Text >WORK</Text>
                     </Text>
                 </View>
             </View>
 
-            <View style={styles.booksContainer}>
-                <Text> Your books will be here </Text> 
+            <View style={styles.rulerStyle}/>
+            <View style={styles.rulerStyle2}/>
+
+            <View style={[styles.container, {paddingTop: 0, margin: 0, flex: 0, marginTop: 15, paddingBottom: 5}]}>
+                <Text style={styles.subHeaderStyle}> Your work</Text>
+            </View>
+
+            <View style={{flex: 6}}>
+                <ScrollView style={styles.scrollContainer}>
+                    {fakebooks && fakebooks.map(item => (
+                        // Add key based on 
+                        <TouchableOpacity key={item.id} onPress={() => viewBook(item)}>
+                            <View key={item.id} style={styles.bookStyle}>
+                                <Image source={require('../assets/images/no_cover_book.png')}/>
+
+                                <View style={styles.bookInfo}>
+                                    <Text style={styles.info}>Title: {item.title}</Text>
+                                </View>
+                            </View>
+                        </TouchableOpacity>
+                    ))}
+                </ScrollView>
             </View>
         </DarkBackgroundS>
     );
@@ -63,13 +117,7 @@ export default function ProfileScreen () {
 const styles = StyleSheet.create({
     infoText: { color: theme.colors.text, fontSize: 17, alignSelf: "center"},
     backStyle: { flex: 0, paddingTop: 25, width: '100%',},
-    
-    container: {
-        flex: 2,
-        backgroundColor: theme.colors.darkBox,
-        borderRadius: 15,
-        paddingTop: 20,
-    },
+    stats: { color: theme.colors.text, margin: 3,},
 
     statsContainer: {
         flex: 0,
@@ -77,18 +125,47 @@ const styles = StyleSheet.create({
         justifyContent: "space-around",
         padding: 10,
     },
-
-    stats: {
-        color: theme.colors.text,
-        margin: 3,
+    
+    container: {
+        flex: 2,
+        backgroundColor: theme.colors.darkBox,
+        borderRadius: 15,
+        paddingTop: 20,
+        marginBottom: 5,
     },
 
-    booksContainer: {
-        flex: 6,
+    scrollContainer: {
+        flex: 5,
+        borderRadius: 15,
+        padding: 10,
     },
 
     rulerStyle: {
         borderBottomColor: 'white',
         borderBottomWidth: StyleSheet.hairlineWidth,
     },
+
+    bookInfo: { padding: 10, }, 
+    info: { paddingBottom: 10, color: theme.colors.text },
+    bookStyle: { paddingBottom: 25, flexDirection: "row", },
+
+    rulerStyle: {
+        borderBottomColor: 'white',
+        paddingBottom: 2,
+        borderRadius: 45,
+        borderBottomWidth: StyleSheet.hairlineWidth,
+    },
+
+    rulerStyle2: {
+        borderBottomColor: theme.colors.primary,
+        paddingBottom: 2,
+        borderBottomWidth: 3,
+        borderRadius: 45,
+    },
+
+    subHeaderStyle : {
+        alignSelf: "center",
+        color: theme.colors.text,
+        fontSize: 18,
+    }
 });
